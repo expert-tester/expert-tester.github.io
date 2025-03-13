5bae4d8576c6a90340ae9ecb31b5463cbd211257
expert-tester <chunon0705@gmail.com>
2025-03-13 15:56:54 +0000

    fix http

--- tools/test.sh
+++ tools/test.sh
@@ -63,7 +63,7 @@ main() {
   # test
   bundle exec htmlproofer "$SITE_DIR" \
     --disable-external \
-    --ignore-urls "/^http:\/\/127.0.0.1/,/^http:\/\/0.0.0.0/,/^http:\/\/localhost,/^http://"
+    --ignore-urls "^http://"
 
 }
 
